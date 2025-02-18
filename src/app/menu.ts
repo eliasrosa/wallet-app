e2eef15b2cc61a9128b2a749b1265f19b5a0092c
Elias da Rosa <elias@eliasdarosa.com.br>
2025-02-17 22:05:54 -0300

    Add theme provider and mode toggle components; update sidebar and layout for theme support

---
+++ src/app/menu.ts
@@ -0,0 +1,61 @@
+import { BetweenHorizonalStartIcon, ChartNoAxesCombinedIcon, ChartPieIcon, FileSpreadsheetIcon, HomeIcon, ShoppingCartIcon, TargetIcon } from "lucide-react"
+
+export const sidebar = {
+  navMain: [
+    {
+      title: "Home",
+      url: "#",
+      items: [
+        {
+          title: "Geral",
+          url: "#",
+          isActive: true,
+          icon: ChartNoAxesCombinedIcon,
+        },
+        {
+          title: "Proventos",
+          url: "#",
+          icon: ChartPieIcon
+        },
+      ],
+    },
+    {
+      title: "Carteira",
+      url: "#",
+      items: [
+        {
+          title: "Ativos",
+          url: "#",
+          icon: HomeIcon
+        },
+        {
+          title: "Metas",
+          url: "#",
+          icon: TargetIcon,
+        },
+        {
+          title: "Comprar",
+          url: "#",
+          icon: ShoppingCartIcon,
+        },
+      ],
+    },
+    {
+      title: "Configurações",
+      url: "#",
+      items: [
+        {
+          title: "Cadastros",
+          url: "#",
+          icon: BetweenHorizonalStartIcon
+        },
+        {
+          title: "Importar B3",
+          url: "#",
+          icon: FileSpreadsheetIcon
+        },
+      ],
+    },
+
+  ],
+}
