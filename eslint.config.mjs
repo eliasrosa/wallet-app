b9a5eea3e8cf2b68180b1e1e175180d592055956
Elias da Rosa <elias@eliasdarosa.com.br>
2025-02-20 22:57:37 -0300

    Add ESLint and Prettier configuration; update package.json scripts and VSCode settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +9,24 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/typescript'),
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          parser: 'flow',
+        },
+      ],
+    },
+  },
 ];
+
+// console.log(JSON.stringify(eslintConfig, null, 2));
 
 export default eslintConfig;
