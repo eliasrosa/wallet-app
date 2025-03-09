ff85373bf62be77876f731c97da4fb3568eb1cc8
Elias da Rosa <elias@eliasdarosa.com.br>
2025-03-09 16:34:33 -0300

    Enhance grid layout in dashboard for improved responsiveness on larger screens

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
 					<SidebarTrigger />
 					<ModeToggle />
 				</header>
-				<div className="grid auto-rows-min gap-4 p-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+				<div className="grid auto-rows-min gap-4 p-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-6">
 					<WalletTickerTypeGoalCard />
 					<WalletBalanceCard />
 					<WalletEvolutionCard className="sm:col-span-2" />
